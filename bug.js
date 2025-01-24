ebd270c311a11d9c900cc82a123c29a2997859bf
BugHunter-X <askzenai@gmail.com>
2025-01-24 21:38:07 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+This error occurs when using the Expo ImagePicker library and selecting an image from the user's device. The image is successfully picked, but when you try to access the image's URI using image.uri, it returns undefined or null, even though image.type is correctly populated.
